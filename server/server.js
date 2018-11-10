498e37310ee48c39b8eb7f94da7d6890c75d22e2
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-11-10 23:38:41 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express()
-const port = 3000
+const port = process.env.port || 3000;
 const request = require('request');
 const router = express.Router();
 const path = require('path');
