34b6b366ebc2c97de60d01bef31e951403ef67c0
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-11-10 22:56:34 +0100

    added input field

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const port = 3000
 const request = require('request');
 const router = express.Router();
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(cookieParser());
 
 
 //app.use(express.static( '../public/'));
@@ -15,26 +21,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
 
 
+app.get('/', (req, res) => {
+    res.render('index', {weather: null, error: null});
+  })
 
 
-
-app.get('/',(req, res) => {
+app.post('/',(req, res) => {
     let url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=52bca177990ef3a0fafe3e3dbef8fbbc';
 
-
-    /*request(url, function (err, response, body) {
-        if(err){
-          res.render('index', {weather: null, error: 'Error, please try again'});
-        } else {
-          let weather = JSON.parse(body)
-          if(weather.main == undefined){
-            res.render('index', {weather: null, error: 'Error, please try again'});
-          } else {
-            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-            res.render('index', {weather: weatherText, error: null});
-          }
-        }
-      });*/
     request(url, (err, response, body) => {
         if(err){
             res.render('index', {weather: null, error: 'Error, please try again'})
@@ -48,6 +42,18 @@ app.get('/',(req, res) => {
 
     })
     }); 
+
+    /*app.get('/weather', (req, res) => {
+        res.render('index');
+    });*/
+
+    /*app.post('/weather', (req, res) => {
+        //res.cookie('city', req.body.city);
+        console.log('click');
+        console.log(req.body.city);
+        res.cookie('city', req.body.city);
+        res.redirect('/');
+    });*/
 
 
 
